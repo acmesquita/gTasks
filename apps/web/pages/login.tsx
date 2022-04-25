a2b1a37d11e5bf15c33b7008365d6690b99c92b1
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-04-25 10:12:18 -0300

    feat: create button to login with github

---
+++ apps/web/pages/login.tsx
@@ -0,0 +1,10 @@
+import { BtnGithub } from "ui";
+
+export default function Login(){
+
+  return (
+    <main>
+      <BtnGithub />
+    </main>
+  )
+}
---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,9 @@
+import style from '../styles/button.module.css'
+
+type Props = {
+  onClick: () => void
+}
+
+export const BtnGithub = (props: Props) => {
+  return <button {...props} className={style.btn}>Login com o Github</button>;
+};
