81ef06fda92f9c5cbfb4a992bd530125fe762d5e
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-05-02 15:51:49 -0300

    feat: removendo opção de criação de usuário vai API

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,13 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import { router as UserRouter } from './routes/user.routes'
 import { router as TasksRouter } from './routes/tasks.routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(UserRouter)
 app.use(TasksRouter)
 
 app.listen(5678, () => {
