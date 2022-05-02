ec49e9e58f1e278f3de42058eb7cf7fa02ca6dfa
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-05-02 17:38:24 -0300

    feat: adicionando estilo

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,14 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
+import { useState } from 'react';
 import { BtnAdd, BtnDel, Form, TextField } from 'ui';
 import { apiLocal } from '../lib/api';
 import styles from '../styles/home.module.css';
 
+type Task = {
+  id: string
+  contant: string
+  done: boolean
+  createdAt: string
+  name: string
+  avatarUrl: string
+}
+
+const tmpTasks = [
+  {
+    id: '1234',
+    contant: "Marcar Academia",
+    avatarUrl: "https://github.com/acmesquita.png",
+    createdAt: "02/05/2022",
+    done: false,
+    name: "Catharina Mesquita"
+  },
+  {
+    id: '3456',
+    contant: "Marcar pra sair para o cinema",
+    avatarUrl: "https://github.com/acmesquita.png",
+    createdAt: "02/05/2022",
+    done: false,
+    name: "Catharina Mesquita"
+  }
+]
 
 export default function Home() {
 
   const { data: session } = useSession()
+  const [tasks, setTasks] = useState<Task[]>(tmpTasks)
 
   async function createTask(content: string) {
     const data = {
@@ -54,25 +83,28 @@ export default function Home() {
           <h2 className={styles.listTitle}>Todo list</h2>
           <ul className={styles.list}>
             <hr className={styles.divider} />
-            <li className={styles.listItem}>
-              <div className={styles.itemText}>
-                <input type="checkbox" name="tasks1" id="tasks1" />
-                <label htmlFor='tasks1'>Marcar um horário na academia</label>
-              </div>
-              <div>
-                <BtnDel />
-              </div>
-            </li>
-            <hr className={styles.divider} />
-            <li className={styles.listItem}>
-              <div className={styles.itemText}>
-                <input type="checkbox" name="tasks2" id="tasks2" />
-                <label htmlFor='tasks2'>Marcar um horário na academia</label>
-              </div>
-              <div>
-                <BtnDel />
-              </div>
-            </li>
+            {tasks.map((task) => {
+              return (
+                <>
+                  <li className={styles.listItem}>
+                    <div className={styles.flex1}>
+                      <div className={styles.itemText}>
+                        <input type="checkbox" name="tasks1" id="tasks1" />
+                        <label htmlFor='tasks1'>{task.contant}</label>
+                      </div>
+                      <div className={styles.itemUser}>
+                        <img src={task.avatarUrl} alt={task.name} />
+                        <small>{`${task.name} ${task.createdAt}`}</small>
+                      </div>
+                    </div>
+                    <div>
+                      <BtnDel />
+                    </div>
+                  </li>
+                  <hr className={styles.divider} />
+                </>
+              )
+            })}
           </ul>
         </div>
       </div>
