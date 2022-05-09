eb7349b38e6b3028ebecfe04543ba905b4490afc
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-05-09 15:44:34 -0300

    refactor: extract routes

--- apps/account/main/factory/userServer.ts
+++ apps/account/main/factory/userServer.ts
@@ -1,39 +1,8 @@
-import { grpc, UserHandlers, userPackage } from 'grpc/configUserPackage'
-import { UserDTO } from '../../domain/dto/user-dto';
-import { createUser, findAllUser, findUser } from './userService';
+import { grpc, userPackage } from 'grpc/configUserPackage'
+import { UserRoutes } from '../routes/user.routes';
 
 export function getUserServer() {
   const server = new grpc.Server()
-  server.addService(userPackage.User.service, {
-    Create: async (call) => {
-      const user = await createUser({
-        name: call.request.name,
-        avatarUrl: call.request.avatarUrl
-      } as UserDTO)
-      if(user) {
-        console.log('usuário cadastrado.')
-        call.write(user)
-      }
-      call.end()
-    },
-    Find: async (call) => {
-      const { id } = call.request
-      const user = await findUser(id as string)
-      if (user) {
-        call.write(user)
-      }
-      call.end()
-    },
-    FindAll: async (call) => {
-      const { ids } = call.request
-      const users = await findAllUser(ids as string[])
-      if (users) {
-        call.write({
-          users
-        })
-      }
-      call.end()
-    }
-  } as UserHandlers)
+  server.addService(userPackage.User.service, new UserRoutes())
   return server;
 }
---
+++ apps/account/main/routes/user.routes.ts
@@ -0,0 +1,54 @@
+import { handleServerStreamingCall } from "@grpc/grpc-js";
+import { UserHandlers } from "grpc/configUserPackage";
+import { UserIdRequest__Output } from "grpc/proto/userPackage/UserIdRequest";
+import { UserIdsRequest__Output } from "grpc/proto/userPackage/UserIdsRequest";
+import { UserRequest__Output } from "grpc/proto/userPackage/UserRequest";
+import { UserResponse } from "grpc/proto/userPackage/UserResponse";
+import { UsersResponse } from "grpc/proto/userPackage/UsersResponse";
+import { UserDTO } from "../../domain/dto/user-dto";
+import * as userService from "../factory/userService";
+
+export class UserRoutes implements UserHandlers {
+
+  [name: string]: import("@grpc/grpc-js").UntypedHandleCall;
+  
+  Create: handleServerStreamingCall<UserRequest__Output, UserResponse> = async (call) => {
+    try {
+      const user = await userService.createUser({
+        name: call.request.name,
+        avatarUrl: call.request.avatarUrl
+      } as UserDTO)
+      call.write(user)
+    }catch(err) {
+      console.log(err)
+    }finally {
+      call.end()
+    }
+  }
+  
+  Find: handleServerStreamingCall<UserIdRequest__Output, UserResponse> = async (call) => {
+    try {
+      const { id } = call.request
+      const user = await userService.findUser(id as string)
+      call.write(user)
+    }catch(err) {
+      console.log(err)
+    }finally {
+      call.end()
+    }
+  }
+  
+  FindAll: handleServerStreamingCall<UserIdsRequest__Output, UsersResponse> = async (call) => {
+    try {
+      const { ids } = call.request
+      const users = await userService.findAllUser(ids as string[])
+      call.write({
+        users
+      })
+    }catch(err) {
+      console.log(err)
+    }finally {
+      call.end()
+    }
+  }
+}
